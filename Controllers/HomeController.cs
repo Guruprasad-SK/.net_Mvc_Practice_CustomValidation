8887bf2d77b58332f4549aa8d1407ddc982dee50
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2023-11-06 16:00:43 +0530

    C#,.netmvc

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CustomValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomValidation.Models;
+
+namespace CustomValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        
+        public ActionResult Index(Employee emp)
+        {
+            if(ModelState.IsValid)
+            {
+                ModelState.Clear();
+                return View();
+            }
+            return View();
+        }
+    }
+}
